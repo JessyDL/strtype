0ba92096168c4cca636844411ff6e8b65a551587
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2022-11-17 17:43:00 +0200

    rewrote better constraints, and extensive overriding

--- include/strenum/strenum.hpp
+++ include/strenum/strenum.hpp
@@ -3,10 +3,33 @@
 #include <string_view>
 #include <type_traits>
 
+#if defined(_MSC_VER)
+	#define STRENUM_MSVC 1
+	#define STRENUM_SIG __FUNCSIG__
+#elif defined(__GNUG__)
+	#define STRENUM_GNUG 1
+	#define STRENUM_SIG __PRETTY_FUNCTION__
+#else
+	#error Either __FUNCSIG__ (MSVC) or __PRETTY_FUNCTION__ (GCC/CLang) required
+#endif
+
+#if !defined(STRENUM_MAX_SEARCH_SIZE)
+	#define STRENUM_MAX_SEARCH_SIZE 1024
+#endif
+
 namespace strenum
 {
+	enum class pattern
+	{
+		UNKNOWN,	// will try to figure out if the type is sequential or bitflag, but it's best to set this manually
+		SEQUENTIAL,	   // do the enum values come sequentially? i.e. 0, 1, 2, 3, ..., 9, 10 .. (does not need to be
+					   // consecutive)
+		FLAG,		   // are the values more akin to bit flags? i.e. 1, 2, 4, 8, 16, ..., 512, 1024 ..
+	};
+
 	namespace details
 	{
+#pragma region fixed_string
 		template <size_t N>
 		struct fixed_string;
 
@@ -35,10 +58,10 @@ namespace strenum
 			constexpr operator std::string_view() const noexcept { return std::string_view {buf, N}; }
 			constexpr operator char const*() const { return buf; }
 
-			constexpr size_t size() const noexcept { return N; }
+			constexpr auto size() const noexcept -> size_t { return N; }
 
 			template <size_t start, size_t end>
-			consteval fixed_string<end - start> substr() const noexcept
+			consteval auto substr() const noexcept -> fixed_string<end - start>
 			{
 				static_assert(start <= end);
 				static_assert(end <= N + 1);
@@ -47,19 +70,18 @@ namespace strenum
 		};
 		template <unsigned N>
 		fixed_string(char const (&)[N]) -> fixed_string<N - 1>;
-
+#pragma endregion fixed_string
+#pragma region helpers
 		consteval auto to_underlying(auto value) { return static_cast<std::underlying_type_t<decltype(value)>>(value); }
-		template <typename T>
-		concept EnumHasKnownBegin = requires()
-		{
-			T::_BEGIN;
-		};
 
 		template <typename T>
-		concept EnumHasKnownEnd = requires()
-		{
-			T::_END;
-		};
+		concept IsValidStringifyableEnum = std::is_enum_v<T> && std::is_integral_v<std::underlying_type_t<T>>;
+
+		template <typename T>
+		concept EnumHasKnownBegin = requires() { T::_BEGIN; };
+
+		template <typename T>
+		concept EnumHasKnownEnd = requires() { T::_END; };
 
 		template <EnumHasKnownBegin T>
 		consteval auto enum_start() -> std::underlying_type_t<T>
@@ -73,14 +95,81 @@ namespace strenum
 			return to_underlying(T::_END) + 1;
 		}
 
-		template <details::fixed_string signature>
-		consteval auto scan_signature_range() -> std::pair<size_t, size_t>
+		template <typename T>
+		struct enum_start_t
+		{};
+
+		template <EnumHasKnownBegin T>
+		struct enum_start_t<T>
 		{
-			if(signature.size() == 0) return {};
-			size_t depth {0};
-			const bool is_funcsig = signature[signature.size() - 1] != ']';
-			if(is_funcsig)
+			static constexpr auto BEGIN = details::enum_start<T>();
+		};
+
+		template <typename T>
+		struct enum_end_t
+		{};
+
+		template <EnumHasKnownEnd T>
+		struct enum_end_t<T>
+		{
+			static constexpr auto END = details::enum_end<T>();
+		};
+
+		// figure out if the Value is either an enum value of the given enum type, or equivalent to the underlying
+		// value;
+		template <typename T, auto Value>
+		concept IsEnumValueOrUnderlying = IsValidStringifyableEnum<T> &&
+										  (std::is_same_v<decltype(Value), T> ||
+										   std::is_same_v<decltype(Value), std::underlying_type_t<T>>);
+
+		template <typename T, auto Value, bool ApplyOffset = false>
+			requires(IsEnumValueOrUnderlying<T, Value>)
+		consteval auto guarantee_is_underlying_value() -> std::underlying_type_t<T>
+		{
+			if constexpr(std::is_same_v<T, decltype(Value)>)
+				return to_underlying(Value) + std::underlying_type_t<T>(ApplyOffset ? 1 : 0);
+			else
+				return Value;
+		}
+#pragma endregion helpers
+	}	 // namespace details
+
+	template <details::IsValidStringifyableEnum T>
+	struct enum_information : public details::enum_start_t<T>, details::enum_end_t<T>
+	{
+		static constexpr pattern PATTERN = pattern::UNKNOWN;
+	};
+
+	namespace details
+	{
+		template <typename T>
+		concept HasMaxSearchSizeOverride = requires() { enum_information<T>::MAX_SEARCH_SIZE; };
+
+		template <typename T>
+		consteval auto max_search_size() -> size_t
+		{
+			if constexpr(HasMaxSearchSizeOverride<T>)
 			{
+				return enum_information<T>::MAX_SEARCH_SIZE;
+			}
+			else
+			{
+				return STRENUM_MAX_SEARCH_SIZE;
+			}
+		}
+
+
+		consteval bool is_named_value(const std::string_view value) { return value.size() > 0 && value.at(0) != '('; }
+		// returns the value of the given enum as a cross platform (MSVC, GCC, and CLang) consistent fixed_string
+		template <auto Value>
+		consteval auto get_enum_value_string()
+		{
+			constexpr auto full_signature = details::fixed_string {STRENUM_SIG};
+
+			constexpr auto range = [&signature = full_signature]() -> std::pair<size_t, size_t> {
+				if(signature.size() == 0) return {};
+				size_t depth {0};
+#if defined(STRENUM_MSVC)
 				size_t end {0};
 				for(auto i = 0; i != signature.size(); ++i)
 				{
@@ -106,9 +195,7 @@ namespace strenum
 						return {index + 1, end};
 					}
 				}
-			}
-			else
-			{
+#elif defined(STRENUM_GNUG)
 				for(auto i = 0; i != signature.size(); ++i)
 				{
 					auto index = (signature.size() - 1) - i;
@@ -118,92 +205,82 @@ namespace strenum
 						return {index + 1, signature.size() - 1};
 					}
 				}
-			}
-			throw std::exception();	   // we couldn't find the start of the signature
-		}
-
-		template <details::fixed_string Str>
-		consteval auto store_str()
-		{
-			return &Str.buf[0];
-		}
-
-		template <auto v>
-		consteval auto stringify_value()
-		{
-#if defined(__FUNCSIG__)
-			constexpr auto signature = details::fixed_string {__FUNCSIG__};
-			constexpr auto pair		 = scan_signature_range<__FUNCSIG__>();
-#elif defined(__GNUG__)
-			constexpr auto signature = details::fixed_string {__PRETTY_FUNCTION__};
-			constexpr auto pair		 = scan_signature_range<__PRETTY_FUNCTION__>();
-#else
-	#error Either __FUNCSIG__ (MSVC) or __PRETTY_FUNCTION__ (GCC/CLang) required
 #endif
-			constexpr auto substr = signature.template substr<pair.first, pair.second>();
-			return store_str<substr>();
+				throw std::exception();	   // we couldn't find the start of the signature
+			}();
+
+			return full_signature.template substr<range.first, range.second>();
 		}
 
-		template <typename T, auto V>
-		consteval bool is_known_enum_value()
-		{
-			return stringify_value<T {V}>()[0] != '(';
-		}
-
-		template <typename T, auto offset, size_t size, size_t... Indices>
-		consteval auto stringify(std::index_sequence<Indices...>)
-		{
-			std::array<std::string_view, size> storage {};
-			size_t index = 0;
-			(
-			  [&index, &storage]() mutable {
-				  if(is_known_enum_value<T, Indices>()) storage[index++] = stringify_value<T {Indices}>();
-			  }(),
-			  ...);
-			return storage;
-		}
-
-		template <typename T, auto offset, size_t... Indices>
-		consteval auto scan_size(std::index_sequence<Indices...>)
+		template <typename T, pattern Pattern, std::underlying_type_t<T> Offset, std::underlying_type_t<T>... Indices>
+		consteval auto get_named_entries_count(std::integer_sequence<std::underlying_type_t<T>, Indices...>) -> size_t
+			requires(Pattern == pattern::SEQUENTIAL)
 		{
 			size_t count = 0;
-			([&count]() mutable { count += is_known_enum_value<T, Indices>() ? 1 : 0; }(), ...);
+			([&count]() mutable { count += is_named_value(get_enum_value_string<T {Indices + Offset}>()); }(), ...);
 			return count;
 		}
 
-		template <typename T, std::underlying_type_t<T> Begin, std::underlying_type_t<T> End>
-		requires(std::is_enum_v<T>&& End >= Begin) consteval auto stringify()
+		template <typename T, auto Begin, auto End, pattern Pattern>
+		consteval auto get_unique_entries()
+			requires(Pattern != pattern::UNKNOWN)
 		{
-			constexpr auto Size = details::scan_size<T, Begin>(std::make_index_sequence<End - Begin>());
+			constexpr auto MAXSIZE = End - Begin;
+			static_assert(
+			  MAXSIZE <= max_search_size<T>(),
+			  "Up the max search size for this enum. Either prefferably by specializing the 'enum_information<T>' and "
+			  "setting MAX_SEARCH_SIZE higher, or by upping the define (which would globally increase it)");
 
-			return details::stringify<T, Begin, Size>(std::make_index_sequence<End - Begin>());
+			constexpr auto size = get_named_entries_count<T, Pattern, Begin>(
+			  std::make_integer_sequence<std::underlying_type_t<T>, MAXSIZE>());
+
+			if constexpr(Pattern == pattern::SEQUENTIAL)
+			{
+				
+
+				return []<size_t Size, std::underlying_type_t<T> Offset, std::underlying_type_t<T>... Indices>(
+				  std::integer_sequence<std::underlying_type_t<T>, Indices...>)
+				{
+					size_t index = 0;
+					std::array<std::string_view, Size> result;
+
+					auto set_value = []<details::fixed_string Str>(size_t index, auto& buffer) { buffer[index] = Str; };
+					(
+					  [&index, &result, set_value]() mutable {
+						  constexpr auto name = get_enum_value_string<T {Indices + Offset}>();
+						  if(is_named_value(name))
+						  {
+							  set_value.template operator()<name>(index++, result);
+						  }
+					  }(),
+					  ...);
+					return result;
+				}
+				.template operator()<size, Begin>(std::make_integer_sequence<std::underlying_type_t<T>, MAXSIZE>());
+			}
+		}
+
+		template <typename T, auto Begin, auto End, pattern Pattern>
+		consteval auto get_unique_entries()
+			requires(Pattern == pattern::UNKNOWN)
+		{
+			return get_unique_entries<T, Begin, End, pattern::SEQUENTIAL>();
 		}
 	}	 // namespace details
 
-	template <typename T, auto Begin = details::enum_start<T>(), auto End = details::enum_end<T>()>
-	requires(std::is_enum_v<T>) consteval auto stringify()
+	template <details::IsValidStringifyableEnum T,
+			  auto Begin	  = enum_information<T>::BEGIN,
+			  auto End		  = enum_information<T>::END,
+			  pattern Pattern = enum_information<T>::PATTERN>
+	consteval auto stringify()
 	{
-		if constexpr(std::is_same_v<decltype(Begin), T> && std::is_same_v<decltype(End), T>)
-		{
-			return details::stringify<T, details::to_underlying(Begin), details::to_underlying(End) + 1>();
-		}
-		else if constexpr(std::is_same_v<decltype(Begin), T> &&
-						  std::is_same_v<decltype(End), std::underlying_type_t<T>>)
-		{
-			return details::stringify<T, details::to_underlying(Begin), End>();
-		}
-		else if constexpr(std::is_same_v<decltype(Begin), std::underlying_type_t<T>> &&
-						  std::is_same_v<decltype(End), T>)
-		{
-			return details::stringify<T, Begin, details::to_underlying(End) + 1>();
-		}
-		else if constexpr(std::is_same_v<decltype(Begin), std::underlying_type_t<T>> &&
-						  std::is_same_v<decltype(End), std::underlying_type_t<T>>)
-		{
-			return details::stringify<T, Begin, End>();
-		}
-		throw std::exception();	   // The values of Begin/End are required to be of the
-								   // same type as the enum, or by equivalent to the
-								   // underlying type
+		constexpr auto begin = details::guarantee_is_underlying_value<T, Begin>();
+		constexpr auto end	 = details::guarantee_is_underlying_value<T, End, true>();
+		static_assert(begin < end, "The end value should be larger than begin");
+		return details::get_unique_entries<T, begin, end, Pattern>();
 	}
 }	 // namespace strenum
+
+#undef STRENUM_MSVC
+#undef STRENUM_GNUG
+#undef STRENUM_SIG
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-enum class foo
+enum class foo_known_size
 {
 	foo,
 	bar,
@@ -13,7 +13,7 @@ enum class foo
 	_END   = tan,
 };
 
-enum class foo2
+enum class foo_with_hole
 {
 	foo,
 	bar,
@@ -22,11 +22,23 @@ enum class foo2
 	tan
 };
 
+enum class bit_ops : unsigned char
+{
+	NONE	   = 0,
+	BIT		   = 1 << 0,
+	SHIFT	   = 1 << 1,
+	ARITHMETIC = 1 << 2,
+	LOGICAL	   = 1 << 3
+};
+
 int main()
 {
-	constexpr auto foo_str	= strenum::stringify<foo, foo::foo>();
-	constexpr auto foo2_str = strenum::stringify<foo2, foo2::foo, foo2::tan>();
+	/*constexpr auto foo_str		= strenum::stringify<foo>();
+	constexpr auto foo2_str		= strenum::stringify<foo2, foo2::foo, foo2::tan>();
+	constexpr auto enum_ops_str = strenum::stringify<enum_ops_t, enum_ops_t::NONE, enum_ops_t::LOGICAL>();
 	for(auto str : foo2_str) std::cout << str << std::endl;
-	for(auto str : foo_str) std::cout << str << std::endl;
+	for(auto str : foo_str) std::cout << str << std::endl;*/
+
+	constexpr auto v = strenum::stringify<bit_ops, bit_ops::NONE, bit_ops::LOGICAL>();
 	return 1;
 }
